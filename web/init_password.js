c641f91d821dc1cc3a58ab17b82bb0fd13b692da
汪洋 <learnwy@gmail.com>
2017-03-16 12:44:42 +0800

    Update init_password.js

--- web/init_password.js
+++ web/init_password.js
@@ -1,6 +1,10 @@
 // c is the keyMap 
 // c 是 生成密钥的字典
-var d=[],f='',c="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`~!@#$%^&*-+=|{}[]:;'<>,.?/123456789".split('');
+
+var aliyun = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`~!@#$%^&*-+=|{}[]:;'<>,.?/0123456789";
+var qcloud = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM()0123456789`~!@#$%^&*-+=_|{}[]:;'<>,.?/"
+var d=[],f='',c=aliyun.split('');
+
 for(let i=0;i<30;i++) {
   f=c[e=~~(Math.random()*c.length)];
   d.push(f);
