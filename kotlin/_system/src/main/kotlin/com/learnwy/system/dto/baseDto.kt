8fa1813cc9dca4c70116e15189972cf31ab68a4c
汪洋 <learnwy@gmail.com>
2017-11-06 02:11:19 +0800

    5

---
+++ kotlin/_system/src/main/kotlin/com/learnwy/system/dto/baseDto.kt
@@ -0,0 +1,18 @@
+package com.learnwy.system.dto
+
+import javax.persistence.Column
+
+open class baseDto {
+    @Column
+    lateinit var attribute1: String;
+    lateinit var attribute2: String;
+    lateinit var attribute3: String;
+    lateinit var attribute4: String;
+    lateinit var attribute5: String;
+    lateinit var attribute6: String;
+    lateinit var attribute7: String;
+    lateinit var attribute8: String;
+    lateinit var attribute9: String;
+    lateinit var attribute10: String;
+    lateinit var attribute11: String;
+}
